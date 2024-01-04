bb2281dbc04d58788d64e47d94028b4e91d978b0
petr valouch <petrdragonman@me.com>
2024-01-05 08:12:24 +1100

    Initial Commit

---
+++ DragDropObject/DragDropObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragDropObjectApp.swift
+//  DragDropObject
+//
+//  Created by labuser on 5/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct DragDropObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
