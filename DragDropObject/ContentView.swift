18910f5b08b1c45e9365fd32a06041e98d6d098a
petr valouch <petrdragonman@me.com>
2024-01-05 08:16:45 +1100

    initial project commit

--- DragDropObject/ContentView.swift
+++ DragDropObject/ContentView.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var toDoTasks: [String] = ["@Observable Migration", "Keyframe Animations", "Migrate to Swift Data"]
+    @State private var inProgressTasks: [String] = []
+    @State private var doneTasks: [String] = []
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack(spacing: 12) {
+            KanbanView(title: "To Do", tasks: toDoTasks)
+            KanbanView(title: "In Progress", tasks: inProgressTasks)
+            KanbanView(title: "Done", tasks: doneTasks)
         }
         .padding()
     }
@@ -22,5 +26,37 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.landscapeRight)
+    }
+}
+
+struct KanbanView: View {
+
+    let title: String
+    let tasks: [String]
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title).font(.footnote.bold())
+
+            ZStack {
+                RoundedRectangle(cornerRadius: 12)
+                    .frame(maxWidth: .infinity)
+                    .foregroundColor(Color(.secondarySystemFill))
+
+                VStack(alignment: .leading, spacing: 12) {
+                    ForEach(tasks, id: \.self) { task in
+                        Text(task)
+                            .padding(12)
+                            .background(Color(uiColor: .secondarySystemGroupedBackground))
+                            .cornerRadius(8)
+                            .shadow(radius: 1, x: 1, y: 1)
+                    }
+
+                    Spacer()
+                }
+                .padding(.vertical)
+            }
+        }
     }
 }
