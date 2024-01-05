57c9697cce8cbc7ad2b514e948261194de096317
petr valouch <petrdragonman@me.com>
2024-01-05 12:55:20 +1100

    completed athlete - class changed to struct

--- DragDropObject/ContentView.swift
+++ DragDropObject/ContentView.swift
@@ -7,12 +7,14 @@
 
 import SwiftUI
 import Algorithms
+import UniformTypeIdentifiers
+
 
 struct ContentView: View {
 
-    @State private var toDoTasks: [String] = ["@Observable Migration", "Keyframe Animations", "Migrate to Swift Data"]
-    @State private var inProgressTasks: [String] = []
-    @State private var doneTasks: [String] = []
+    @State private var toDoTasks: [Athlete] = [MockData.athlete1, MockData.athlete2, MockData.athlete3]
+    @State private var inProgressTasks: [Athlete] = []
+    @State private var doneTasks: [Athlete] = []
     @State private var isInProgressTargeted: Bool = false
     @State private var isToDoTargeted: Bool = false
     @State private var isDoneTargeted: Bool = false
@@ -20,10 +22,10 @@ struct ContentView: View {
     var body: some View {
         HStack(spacing: 12) {
             KanbanView(title: "To Do", tasks: toDoTasks, isTargeted: isToDoTargeted)
-                .dropDestination(for: String.self) { droppedTasks, location in
+                .dropDestination(for: Athlete.self) { droppedTasks, location in
                     for task in droppedTasks {
-                        inProgressTasks.removeAll(where: {$0 == task})
-                        doneTasks.removeAll(where: {$0 == task})
+                        inProgressTasks.removeAll(where: {$0.id == task.id})
+                        doneTasks.removeAll(where: {$0.id == task.id})
                     }
                     let totalTasks = toDoTasks + droppedTasks
                     toDoTasks = Array(totalTasks.uniqued())
@@ -33,10 +35,10 @@ struct ContentView: View {
                 }
             
             KanbanView(title: "In Progress", tasks: inProgressTasks, isTargeted: isInProgressTargeted)
-                .dropDestination(for: String.self) { droppedTasks, location in
+                .dropDestination(for: Athlete.self) { droppedTasks, location in
                     for task in droppedTasks {
-                        toDoTasks.removeAll(where: {$0 == task})
-                        doneTasks.removeAll(where: {$0 == task})
+                        toDoTasks.removeAll(where: {$0.id == task.id})
+                        doneTasks.removeAll(where: {$0.id == task.id})
                     }
                     let totalTasks = inProgressTasks + droppedTasks
                     inProgressTasks = Array(totalTasks.uniqued())
@@ -46,10 +48,10 @@ struct ContentView: View {
                 }
             
             KanbanView(title: "Done", tasks: doneTasks, isTargeted: isDoneTargeted)
-                .dropDestination(for: String.self) { droppedTasks, location in
+                .dropDestination(for: Athlete.self) { droppedTasks, location in
                     for task in droppedTasks {
-                        inProgressTasks.removeAll(where: {$0 == task})
-                        toDoTasks.removeAll(where: {$0 == task})
+                        inProgressTasks.removeAll(where: {$0.id == task.id})
+                        toDoTasks.removeAll(where: {$0.id == task.id})
                     }
                     let totalTasks = doneTasks + droppedTasks
                     doneTasks = Array(totalTasks.uniqued())
@@ -57,7 +59,6 @@ struct ContentView: View {
                 } isTargeted: { isTargeted in
                     isDoneTargeted = isTargeted
                 }
-            
         }
         .padding()
     }
@@ -73,7 +74,7 @@ struct ContentView_Previews: PreviewProvider {
 struct KanbanView: View {
 
     let title: String
-    let tasks: [String]
+    let tasks: [Athlete]
     let isTargeted: Bool
 
     var body: some View {
@@ -86,8 +87,8 @@ struct KanbanView: View {
                     .foregroundColor(isTargeted ? .green.opacity(0.2) : Color(.secondarySystemFill))
 
                 VStack(alignment: .leading, spacing: 12) {
-                    ForEach(tasks, id: \.self) { task in
-                        Text(task)
+                    ForEach(tasks, id: \.id) { task in
+                        Text(task.name)
                             .padding(12)
                             .background(Color(uiColor: .secondarySystemGroupedBackground))
                             .cornerRadius(8)
@@ -101,4 +102,35 @@ struct KanbanView: View {
             }
         }
     }
+}
+
+struct Athlete: Codable, Hashable, Transferable {
+    
+    static func == (lhs: Athlete, rhs: Athlete) -> Bool {
+        return lhs.id == rhs.id && lhs.name == rhs.name && lhs.weight == rhs.weight
+    }
+    
+    let id: UUID
+    let name: String
+    let weight: Int
+    
+    init(id: UUID, name: String, weight: Int) {
+        self.id = id
+        self.name = name
+        self.weight = weight
+    }
+    
+    static var transferRepresentation: some TransferRepresentation {
+        CodableRepresentation(contentType: .athlete)
+    }
+}
+
+extension UTType {
+    static let athlete = UTType(exportedAs: "com.petrvalouch.athlete")
+}
+
+struct MockData {
+    static let athlete1 = Athlete(id: UUID(), name: "Petr Valouch", weight: 72)
+    static let athlete2 = Athlete(id: UUID(), name: "Jocelyn", weight: 65)
+    static let athlete3 = Athlete(id: UUID(), name: "George", weight: 80)
 }
